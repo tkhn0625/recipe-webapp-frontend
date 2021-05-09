5889194f69adfcb8765ec9c93cd930e32d5cc870
tkhn0625 <athleta.0315@gmail.com>
2021-05-09 12:10:38 +0900

    create recipeItem component and render to recipeList

---
+++ src/api/RecipeDataService.ts
@@ -0,0 +1,30 @@
+import axios,{AxiosError} from 'axios'
+import {API_URL} from '../Constants'
+
+// class RecipeDataService{
+//     retrieveAllRecipes=()=>{
+//         return axios.get(`${API_URL}/recipes/list`)
+//     }
+
+//     retrieveRecipe(id: string){
+//         return axios.get(`${API_URL}/recipes/${id}`);
+//     }
+// }
+
+// export default new RecipeDataService();
+interface IRecipe{
+  recipes: Array<Recipes>
+}
+
+// レスポンスの型定義。
+interface IResponse {
+    data: IRecipe;
+    error: AxiosError;
+    loading: boolean;
+}
+
+export const retrieveAllRecipes = async ():Promise<IRecipe> =>{
+  const res = await axios.get<IRecipe>(`${API_URL}/recipes/list`);
+  const data = res.data
+  return data;
+}
--- src/components/list/RecipeList.tsx
+++ src/components/list/RecipeList.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import {retrieveAllRecipes} from '../../api/RecipeDataService'
+import RecipeItem from '../list/RecipeItem'
 
 const RecipeList = () => {
+
+  const [recipeItem, setItem] = useState<Recipes[]>(
+    [{
+      id: 0,
+      name: "",
+      createdAt: "",
+      updatedAt: "",
+      mainImages: [{
+                    id: 0,url: "",recipeId: ""
+                  }]
+    }]
+  );
+  useEffect(()=>{
+    retrieveAllRecipes().then((recipes)=>{
+      setItem(recipes.recipes)
+    })
+  })
   return (
     <div>
-      レシピ一覧
+      {
+        recipeItem.map((recipe,index)=>{
+          return (
+          <RecipeItem key={index} getRecipes = {recipe} />
+        )})
+      }
     </div>
   );
 };
